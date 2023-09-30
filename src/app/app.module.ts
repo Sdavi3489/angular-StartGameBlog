0d2d14b4db03df335f928c146e286b1f3a18aa59
Sdavi3489 <sdavi3489@gmail.com>
2023-09-30 19:46:17 -0300

    adding new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BigCardComponent,
+    CardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
